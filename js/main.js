a5f348be20aafade777889b66f73de1d9aa8c3c3
Matvey Kolosov <118886359+MKLSV@users.noreply.github.com>
2022-12-06 08:54:45 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,80 @@
-console.log('Starting up');
+'use strict'
+
+$(document).ready(init)
+function init() {
+    renderProjs()
+}
+function renderProjs() {
+    var modalId = 1
+    const projs = getProjects()
+
+    var strHTML = projs.map(project =>
+        `<div class="col-md-4 col-sm-6 portfolio-item">
+        <a class="portfolio-link" data-toggle="modal")" href="#portfolioModal${modalId++}">
+        <div class="portfolio-hover">
+        <div class="portfolio-hover-content">
+        <i class="fa fa-plus fa-3x"></i>
+        </div>
+        </div>
+        <img class="img-fluid" src="img/portfolio/${project.id}-thumbnail.jpg" alt="">
+        </a>
+        <div class="portfolio-caption">
+        <h4>${project.name}</h4>
+        <p class="text-muted">${project.title}</p>
+        </div>
+        </div>`
+    )
+    $('.my-portfolio').html(strHTML.join(''))
+
+   modalId = 1
+   var strModalHTML = projs.map(project =>  `<div class="portfolio-modal modal fade" id="portfolioModal${modalId++}" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="close-modal" data-dismiss="modal">
+          <div class="lr">
+            <div class="rl"></div>
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 mx-auto">
+              <div class="modal-body">
+                <!-- Project Details Go Here -->
+                <h2>${project.name}</h2>
+                <p class="item-intro text-muted">${project.title}</p>
+                <img class="img-fluid d-block mx-auto" src="img/portfolio/${project.id}-full.jpg" alt="">
+                <p>${project.desc}</p>
+                <a href="${project.url}">${project.url}</a>
+                <hr>
+                <ul class="list-inline">
+                  <li>Date: ${project.publishedAt}</li>
+                  <li>Client: ${project.client}</li>
+                  <li>Category: ${project.category}</li>
+                </ul>
+                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                    <i class="fa fa-times"></i>
+                    Close Project</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`
+   )
+   $('.modal-box').html(strModalHTML.join(''))
+}
+
+function onContact(){
+    $('.email-error').hide()
+    const elEmail = $(".email").val()
+    const elSubject = $(".subject").val()
+    const elMessage = $(".message").val()
+    if(elEmail.includes('@') && elEmail.includes('.')) {
+        window.open(`https://mail.google.com/mail/u/0/?fs=1&to=mkolosov96@gmail.com&su=${elSubject}&body=${elMessage}&bcc=${elEmail}&tf=cm`)
+        elEmail =''
+        elSubject =''
+        elMessage =''
+    }
+    else $('.email-error').show()
+}
---
+++ js/service.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const gProjs = [
+    {
+        id: "01",
+        name: "To do list",
+        title: "Simple To do list",
+        desc: "Soon!! Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!",
+        url: " https://mklsv.github.io/todo-list/",
+        publishedAt: "November 2022",
+        client: "Coding academy",
+        category: "MVC keyboard events",
+    },
+    {
+        id: "02",
+        name: "Book Store",
+        title: "Book Store App",
+        desc: "Soon!! Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!",
+        url: "https://mklsv.github.io/book-shop/",
+        publishedAt: "November 2022",
+        client: "Coding academy",
+        category: "CRUD store",
+    },
+    {
+        id: "03",
+        name: "Minesweeper",
+        title: "Minesweeper game",
+        desc: "Soon!! Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!",
+        url: " https://mklsv.github.io/Minesweeper/",
+        publishedAt: "November 2022",
+        client: "Coding academy",
+        category: "Matrixes Game",
+    },
+    {
+        id: "04",
+        name: "Touch Nums",
+        title: "Touch Nums Game",
+        desc: "Soon!! Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!",
+        url: "https://mklsv.github.io/touch-nums/",
+        publishedAt: "November 2022",
+        client: "Coding academy",
+        category: "Matrixes Game",
+    },
+    {
+        id: "05",
+        name: "PacMan",
+        title: "PacMan game ",
+        desc: "Soon!! Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!",
+        url: "https://mklsv.github.io/my-pacman/",
+        publishedAt: "November 2022",
+        client: "Coding academy",
+        category: "Matrixes Game",
+    },
+    {
+        id: "06",
+        name: "Guess Who",
+        title: "Guess a character",
+        desc: "Soon!! Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate, maiores repudiandae, nostrum, reiciendis facere nemo!",
+        url: "https://mklsv.github.io/guess-me/",
+        publishedAt: "December 2022",
+        client: "Coding academy",
+        category: "Keyboard event",
+    },
+]
+
+function getProjects(){
+    return gProjs
+}
